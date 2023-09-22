3c9a98e54ace0bee8f3ad38fd091b52136ce261b
Eric-Mai-25 <ericmai2@gmail.com>
2023-09-22 16:39:54 -0700

    Art tile started

---
+++ art-museum/src/components/ArtImageTile/index.js
@@ -0,0 +1,17 @@
+// console.log(gallery.objects[0].images[0].baseimageurl)\
+import { Link, useParams } from "react-router-dom";
+
+
+const ArtImageTile = ({ art, galleryId }) => {
+    // console.log(art)
+    // const {artId} = useParams()
+  return (
+    <>
+    <Link to={`/galleries/${galleryId}/art/${art.id}`}>
+      <img src={art.images[0].baseimageurl}></img>
+    </Link>
+    </>
+  );
+};
+
+export default ArtImageTile;
--- art-museum/src/components/GalleryView/index.js
+++ art-museum/src/components/GalleryView/index.js
@@ -1,18 +1,29 @@
 import { useParams } from "react-router-dom";
+import ArtImageTile from "../ArtImageTile";
+import { Route } from "react-router-dom";
 
-const GalleryView = ({galleries}) =>  {
-    const { galleryId } = useParams();
+const GalleryView = ({ galleries }) => {
+  const { galleryId } = useParams();
 
-    const gallery = galleries.find((gallery) => {
-        return gallery.gallerynumber === galleryId;
-    });
+  const gallery = galleries.find((gallery) => {
+    return gallery.gallerynumber === galleryId;
+  });
 
-    return (
-        <>
-            <h1>Hello from GalleryView</h1>
-            <h2>{gallery.name}</h2>
-        </>
-    );
-}
+  const artWorks = Object.values(gallery.objects).map((art) => {
+    if (art.images[0]) {
+      return <ArtImageTile galleryId={galleryId} art={art} />;
+    }
+  });
+
+  return (
+    <>
+      <h1>Hello from GalleryView</h1>
+      <h2>{gallery.name}</h2>
+      <Route exact path={`/galleries/${galleryId}`}>
+        {artWorks}
+        </Route>
+    </>
+  );
+};
 
 export default GalleryView;
